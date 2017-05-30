b76af789fe51d2b1c94bd756fca38cfa0a0af28d
Florian 'adlerweb' Knodt <git@adlerweb.info>
2017-05-30 10:55:31 +0200

    mysql.wrapper: path-agnostic

--- lib/mysql.wrapper.php
+++ lib/mysql.wrapper.php
@@ -1,5 +1,5 @@
 <?php
-    require_once('vendor/adlerweb/awtools/mysql.stmt.php');
+    require_once(realpath(dirname(__FILE__)).'/../vendor/adlerweb/awtools/mysql.stmt.php');
 
     if(!defined('AW_SQL_NOAUTO')) {
         $GLOBALS['adlerweb']['sql'] = new ATK_mysql(AW_SQL_SERV,AW_SQL_USER,AW_SQL_PASS,AW_SQL_DATB);
